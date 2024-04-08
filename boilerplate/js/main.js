d2a57f68d3e0a42ea8214a29e022f6fa1db2eb78
ashmitadhakal <adhakal2@wisc.edu>
2024-04-07 20:52:23 -0500

    Activity 10
    Coordinated Visualization

--- boilerplate/js/main.js
+++ boilerplate/js/main.js
@@ -11,8 +11,8 @@
     //set uo chloropleth map
     function setMap(){
         //map frame dimensions
-        var width = 1100,
-            height = 690;
+        var width = window.innerWidth*0.45,
+            height = 400;
 
         //create new svg container for map
         var map = d3.select("body")
@@ -67,8 +67,116 @@
 
             //add enumeration units to the map
             setEnumerationUnits(nepalProvince, map, path, colorScale);
+
+            setChart(csvData, colorScale);
         };
     };
+
+    //function to create a coordinated bar chart
+    function setChart(csvData, colorScale){
+        //chart frame dimensions
+        //chart frame dimensions
+        var chartWidth = window.innerWidth * 0.425,
+        chartHeight = 473,
+        leftPadding = 25,
+        rightPadding = 2,
+        topBottomPadding = 5,
+        chartInnerWidth = chartWidth - leftPadding - rightPadding,
+        chartInnerHeight = chartHeight - topBottomPadding * 2,
+        translate = "translate(" + leftPadding + "," + topBottomPadding + ")";
+
+        //create a second svg element to hold the bar chart
+        var chart = d3.select("body")
+            .append("svg")
+            .attr("width", chartWidth)
+            .attr("height", chartHeight)
+            .attr("class", "chart");
+
+        //create a rectangle for chart background fill
+        var chartBackground = chart.append("rect")
+            .attr("class", "chartBackground")
+            .attr("width", chartInnerWidth)
+            .attr("height", chartInnerHeight)
+            .attr("transform", translate);
+
+        //create a scale to size bars proportionally to frame and for axis
+        var yScale = d3.scaleLinear()
+            .range([463, 0])
+            .domain([0, 80]);
+
+        //set bars for each province
+        var bars = chart.selectAll(".bar")
+            .data(csvData)
+            .enter()
+            .append("rect")
+            .sort(function(a, b){
+                return b[expressed]-a[expressed]
+                })
+            .attr("class", function(d){
+                return "bar " + d.Provinces;
+            })
+            .attr("width", chartInnerWidth / csvData.length - 1)
+            .attr("x", function(d, i){
+                return i * (chartInnerWidth / csvData.length) + leftPadding;
+            })
+            .attr("height", function(d, i){
+                return 463 - yScale(parseFloat(d[expressed]));
+            })
+            .attr("y", function(d, i){
+                return yScale(parseFloat(d[expressed])) + topBottomPadding;
+            })
+            .style("fill", function(d){
+                return colorScale(d[expressed]);
+            });
+
+        //create a text element for the chart title
+        var chartTitle = chart.append("text")
+            .attr("x", 40)
+            .attr("y", 40)
+            .attr("class", "chartTitle")
+            .text("Number of Technical Schools in Each Province of Nepal");
+
+        //create vertical axis generator
+        var yAxis = d3.axisLeft()
+            .scale(yScale);
+
+        //place axis
+        var axis = chart.append("g")
+            .attr("class", "axis")
+            .attr("transform", translate)
+            .call(yAxis);
+
+        //create frame for chart border
+        var chartFrame = chart.append("rect")
+            .attr("class", "chartFrame")
+            .attr("width", chartInnerWidth)
+            .attr("height", chartInnerHeight)
+            .attr("transform", translate);
+
+        //annotate bars with attribute value text
+        var numbers = chart.selectAll(".numbers")
+            .data(csvData)
+            .enter()
+            .append("text")
+            .sort(function(a, b){
+                return b[expressed]-a[expressed]
+            })
+            .attr("class", function(d){
+                return "numbers " + d.provinces;
+            })
+            .attr("text-anchor", "middle")
+            .attr("x", function(d, i){
+                return i * (chartInnerWidth / csvData.length) + leftPadding + (chartInnerWidth / csvData.length - 1) / 2;
+            })
+            .attr("y", function(d){
+                var barHeight = 463 - yScale(parseFloat(d[expressed]));
+                return chartHeight - barHeight + 15;
+            })
+            .text(function(d){
+                return d[expressed];
+        });
+        };
+    
         function setGraticule(map, path){
             var graticule = d3.geoGraticule().step([5, 5]);
             
@@ -116,11 +224,13 @@
         //function to create color scale generator
         function makeColorScale(data) {
             var colorClasses = [
-                "#D4B9DA",
-                "#C994C7",
-                "#DF65B0",
-                "#DD1C77",
-                "#980043"
+                "#feedde",
+                "#fdd0a2",
+                "#fdae6b",
+                "#fd8d3c",
+                "#f16913",
+                "#d94801",
+                "#8c2d04"
             ];
 
             //create color scale generator
@@ -146,10 +256,6 @@
                 .attr("class", function (d){
                     return "regions" + d.properties.Name;
                 })
-                .attr("d", path)
-                .style("fill", function(d){
-                    return colorScale(d.properties[expressed]);
-                })
                 .attr("d", path)        
                 .style("fill", function(d){            
                     var value = d.properties[expressed];            
@@ -159,6 +265,5 @@
                         return "#ccc";            
                     }  
                 })
-        };
-        
+        }; 
 })();
